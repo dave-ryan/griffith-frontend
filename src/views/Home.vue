bad699fdcb8f5dcd321bddcd4d7a4d47963cd874
dave-a-ryan <daveryan145@gmail.com>
2022-04-20 17:14:16 -0500

    now hides/shows the two views of users, so there won't be any confusion, and lists update upon button press

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,125 +7,157 @@
       <div v-if="loaded">
         <div class="row">
           <div class="col-12 mb-5">
-            <button @click="getEveryone" class="btn btn-outline-primary">
+            <button
+              @click="getEveryone"
+              class="btn btn-outline-primary"
+              v-if="!indexview"
+            >
               Load everyone's lists
             </button>
-            <div
-              class="row mt-3"
-              v-for="user in everyone"
-              :key="`everyone-` + user.id"
+            <button
+              @click="getUsers"
+              class="btn btn-outline-primary"
+              v-if="indexview"
             >
-              <div class="col-12-">
-                <button
-                  class="btn btn-outline-success"
-                  data-bs-toggle="collapse"
-                  :data-bs-target="`#birthday-list-${user.id}`"
-                >
-                  {{ user.name }}
-                </button>
-                <i
-                  class="bi bi-check-lg text-success"
-                  v-if="
-                    user.wishedgifts &&
-                    user.wishedgifts.some((item) => item.purchaser_id == me.id)
-                  "
-                ></i>
-                <div
-                  class="collapse"
-                  aria-expanded="false"
-                  :id="`birthday-list-${user.id}`"
-                >
-                  <span v-if="user.wishedgifts.length < 1"
-                    >this person hasn't made their christmas list yet! remind
-                    them! :)</span
+              Load your family's list
+            </button>
+          </div>
+
+          <div class="col-12">
+            <div v-if="indexview" class="m">
+              <h3>Everyone</h3>
+
+              <div
+                class="row mt-3"
+                v-for="user in everyone"
+                :key="`everyone-` + user.id"
+              >
+                <div class="col-12-">
+                  <button
+                    class="btn btn-outline-success"
+                    data-bs-toggle="collapse"
+                    :data-bs-target="`#birthday-list-${user.id}`"
                   >
+                    {{ user.name }}
+                  </button>
+                  <i
+                    class="bi bi-check-lg text-success"
+                    v-if="
+                      user.wishedgifts &&
+                      user.wishedgifts.some(
+                        (item) => item.purchaser_id == me.id
+                      )
+                    "
+                  ></i>
                   <div
-                    v-for="item in user.wishedgifts"
-                    :key="`everyitem` + item.id"
+                    class="collapse"
+                    aria-expanded="false"
+                    :id="`birthday-list-${user.id}`"
                   >
-                    <div v-if="item.purchaser && item.purchaser_id != me.id">
-                      <div class="form-check form-check-inline">
-                        <input
-                          class="form-check-input"
-                          type="checkbox"
-                          value=""
-                          checked
-                          :id="`birthday-checkbox-` + item.id"
-                          disabled
-                        />
-                        <label
-                          class="form-check-label"
-                          :for="`birthday-checkbox-` + item.id"
-                          :id="`label-` + item.id"
-                        >
-                          {{ item.name }}
-                          <span v-if="item.link || item.purchaser_id"> - </span>
-                          <a
-                            v-if="item.link"
-                            :href="`//` + item.link.replace(/^https?:\/\//, '')"
-                            target="_blank"
-                            >link</a
-                          >
-                        </label>
-                        <span class="text-danger">
-                          purchased by {{ item.purchaser.name }}
-                        </span>
-                      </div>
-                    </div>
-                    <div
-                      v-else-if="
-                        item.purchaser_id && item.purchaser_id == me.id
-                      "
+                    <span v-if="user.wishedgifts.length < 1"
+                      >this person hasn't made their christmas list yet! remind
+                      them! :)</span
                     >
-                      <div class="form-check form-check-inline">
-                        <input
-                          class="form-check-input"
-                          type="checkbox"
-                          value=""
-                          checked
-                          @click="toggleCheckBox(item)"
-                          :id="`birthday-checkbox-` + item.id"
-                        />
-                        <label
-                          class="form-check-label"
-                          :for="`birthday-checkbox-` + item.id"
-                          :id="`label-` + item.id"
-                        >
-                          {{ item.name }}
-                          <span v-if="item.link || item.purchaser_id"> - </span>
-                          <a
-                            v-if="item.link"
-                            :href="`//` + item.link.replace(/^https?:\/\//, '')"
-                            target="_blank"
-                            >link</a
+                    <div
+                      v-for="item in user.wishedgifts"
+                      :key="`everyitem` + item.id"
+                    >
+                      <div v-if="item.purchaser && item.purchaser_id != me.id">
+                        <div class="form-check form-check-inline">
+                          <input
+                            class="form-check-input"
+                            type="checkbox"
+                            value=""
+                            checked
+                            :id="`birthday-checkbox-` + item.id"
+                            disabled
+                          />
+                          <label
+                            class="form-check-label"
+                            :for="`birthday-checkbox-` + item.id"
+                            :id="`label-` + item.id"
                           >
-                        </label>
-                        <span class="text-success"> purchased by you!</span>
+                            {{ item.name }}
+                            <span v-if="item.link || item.purchaser_id">
+                              -
+                            </span>
+                            <a
+                              v-if="item.link"
+                              :href="
+                                `//` + item.link.replace(/^https?:\/\//, '')
+                              "
+                              target="_blank"
+                              >link</a
+                            >
+                          </label>
+                          <span class="text-danger">
+                            purchased by {{ item.purchaser.name }}
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                    <div v-else>
-                      <div class="form-check form-check-inline">
-                        <input
-                          class="form-check-input"
-                          type="checkbox"
-                          value=""
-                          @click="toggleCheckBox(item)"
-                          :id="`checkbox-` + item.id"
-                        />
-                        <label
-                          class="form-check-label"
-                          :for="`checkbox-` + item.id"
-                          :id="`label-` + item.id"
-                        >
-                          {{ item.name }}
-                          <span v-if="item.link || item.purchaser_id"> - </span>
-                          <a
-                            v-if="item.link"
-                            :href="`//` + item.link.replace(/^https?:\/\//, '')"
-                            target="_blank"
-                            >link</a
+                      <div
+                        v-else-if="
+                          item.purchaser_id && item.purchaser_id == me.id
+                        "
+                      >
+                        <div class="form-check form-check-inline">
+                          <input
+                            class="form-check-input"
+                            type="checkbox"
+                            value=""
+                            checked
+                            @click="toggleCheckBox(item)"
+                            :id="`birthday-checkbox-` + item.id"
+                          />
+                          <label
+                            class="form-check-label"
+                            :for="`birthday-checkbox-` + item.id"
+                            :id="`label-` + item.id"
                           >
-                        </label>
+                            {{ item.name }}
+                            <span v-if="item.link || item.purchaser_id">
+                              -
+                            </span>
+                            <a
+                              v-if="item.link"
+                              :href="
+                                `//` + item.link.replace(/^https?:\/\//, '')
+                              "
+                              target="_blank"
+                              >link</a
+                            >
+                          </label>
+                          <span class="text-success"> purchased by you!</span>
+                        </div>
+                      </div>
+                      <div v-else>
+                        <div class="form-check form-check-inline">
+                          <input
+                            class="form-check-input"
+                            type="checkbox"
+                            value=""
+                            @click="toggleCheckBox(item)"
+                            :id="`checkbox-` + item.id"
+                          />
+                          <label
+                            class="form-check-label"
+                            :for="`checkbox-` + item.id"
+                            :id="`label-` + item.id"
+                          >
+                            {{ item.name }}
+                            <span v-if="item.link || item.purchaser_id">
+                              -
+                            </span>
+                            <a
+                              v-if="item.link"
+                              :href="
+                                `//` + item.link.replace(/^https?:\/\//, '')
+                              "
+                              target="_blank"
+                              >link</a
+                            >
+                          </label>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -134,8 +166,7 @@
             </div>
           </div>
 
-          <hr />
-          <div class="col-12">
+          <div class="col-12" v-if="!indexview">
             <h3>Your family</h3>
             <div class="row mt-3" v-for="user in family" :key="user.id">
               <div class="col-12-">
@@ -254,7 +285,7 @@
             </div>
           </div>
         </div>
-        <div class="row mb-5" v-if="secretSanta">
+        <div class="row mb-5" v-if="secretSanta && !indexview">
           <div class="col-12">
             <hr />
             <h3 class="mt-3 mb-4">Your secret santa</h3>
@@ -387,6 +418,7 @@ export default {
       secretSanta: null,
       christmasLists: {},
       loaded: false,
+      indexview: false,
       me: null,
     };
   },
@@ -416,6 +448,7 @@ export default {
       }
     },
     getEveryone: function () {
+      this.indexview = true;
       axios.get("/users").then((response) => {
         console.log(response.data);
         var my_id = this.me.id;
@@ -425,6 +458,7 @@ export default {
       });
     },
     getUsers: function () {
+      this.indexview = false;
       axios
         .get(`/families/${localStorage.family_id}`)
         .then((response) => {
