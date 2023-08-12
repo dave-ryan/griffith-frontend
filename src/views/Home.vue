db7822ad58ef3170f84332318b86f935a2fe9baf
David Ryan <88855799+dave-ryan@users.noreply.github.com>
2023-08-12 16:22:48 -0500

    semi-working customgifts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,11 +59,41 @@
                         aria-expanded="false"
                         :id="`christmas-list-${user.id}`"
                       >
+                        <div>
+                          <input
+                            :id="user.id"
+                            class="form-check-input"
+                            type="checkbox"
+                            @change="toggleCustomGiftCheckBox($event, user)"
+                            :checked="findCustomGift(user)"
+                          />
+                          I'll get them something <strong>not</strong> on their
+                          list
+                          <span v-if="findCustomGift(user)">
+                            -
+                            <button
+                              type="button"
+                              class="btn btn-primary btn-sm"
+                              data-bs-toggle="modal"
+                              data-bs-target="#customGiftModal"
+                              @click="setEditingCustomGift(user)"
+                            >
+                              Edit
+                            </button>
+
+                            -
+                            {{
+                              findCustomGift(user).note?.length > 0
+                                ? findCustomGift(user).note
+                                : "Custom Gift"
+                            }}
+                          </span>
+                        </div>
+                        <br />
                         <span v-if="user.wishedgifts.length < 1"
                           >{{ user.name }} hasn't made a christmas list yet!
                           Remind them! &#128578;</span
                         >
-
                         <WishList
                           v-for="item in user.wishedgifts"
                           :key="item.id"
@@ -100,7 +130,6 @@
           </div>
 
           <!-- Secret Santa Section -->
-
           <div class="row" v-if="secretSanta && !indexview">
             <div class="row">
               <div class="col-2 col-sm-3 col-md-4"></div>
@@ -175,6 +204,62 @@
         </div>
       </div>
     </div>
+
+    <!-- Note Modal -->
+    <div
+      class="modal fade"
+      id="customGiftModal"
+      tabindex="-1"
+      data-bs-backdrop="static"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div>Buying something not on their list...</div>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <div class="modal-body">
+            <label class="input-group-text">What are you geting them?</label>
+            <div class="input-group mb-2">
+              <input
+                type="text"
+                v-model="editingCustomGift.note"
+                class="form-control"
+                required
+                id="newItemName"
+              />
+              <div class="invalid-feedback">
+                A note of what you are getting them might be helpful!
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <!-- <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button> -->
+
+            <button
+              type="submit"
+              class="btn btn-success"
+              data-bs-dismiss="modal"
+              @click="updateCustomGift()"
+            >
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -196,6 +281,7 @@
 import axios from "axios";
 import { Toast } from "bootstrap";
 import WishList from "../components/WishList.vue";
+import { Modal } from "bootstrap";
 
 export default {
   components: { WishList },
@@ -211,6 +297,7 @@ export default {
       splashLoaded: false,
       pageLoaded: false,
       lowPresentCount: false,
+      editingCustomGift: {},
       errorMessage: "",
       defaultErrorMessage:
         "Oops! Something went wrong. Try refreshing the page",
@@ -239,6 +326,11 @@ export default {
       });
   },
   methods: {
+    findCustomGift(user) {
+      return user.customgifts.find(
+        (item) => item.customgift_purchaser_id === this.me.id
+      );
+    },
     getEveryone: function () {
       window.scrollTo({ top: 0, behavior: "smooth" });
       this.indexview = true;
@@ -282,7 +374,46 @@ export default {
           console.log("errors: ", errors.response.data.errors);
         });
     },
-    //todo: add loading wheel while we wait for patching to complete
+    setEditingCustomGift(gift) {
+      // let gift = this.findCustomGift(user);
+      this.editingCustomGift = {
+        id: gift.id,
+        note: gift.note,
+        user_id: gift.user_id,
+        customgift_purchaser: gift.customgift_purchaser,
+        customgift_purchaser_id: gift.customgift_purchaser_id,
+      };
+      this.customGiftCopy = gift;
+    },
+    toggleCustomGiftCheckBox: function (event, user) {
+      if (event.target.checked) {
+        //create
+        let params = {
+          user_id: user.id,
+          customgift_purchaser_id: this.me.id,
+          note: "",
+        };
+        axios.post("/customgifts", params).then((response) => {
+          console.log(response);
+          if (response.status === 200) {
+            this.setEditingCustomGift(response.data);
+            user.customgifts.push(response.data);
+            let myModal = new Modal(
+              document.getElementById("customGiftModal"),
+              {}
+            );
+            myModal.show();
+          }
+        });
+      } else {
+        //destroy
+        let gift = this.findCustomGift(user);
+        axios.delete(`/customgifts/${gift.id}`).then((response) => {
+          console.log(response);
+          gift.customgift_purchaser_id = null;
+        });
+      }
+    },
     toggleCheckBox: function (item) {
       item.loading = true;
       let purchasing = !item.purchaser_id ? true : false;
@@ -323,6 +454,28 @@ export default {
         })
         .catch((errors) => {
           console.log("errors: ", errors.response.data.errors);
+        });
+    },
+    updateCustomGift() {
+      let id = this.editingCustomGift.id;
+      let params = this.editingCustomGift;
+      let user = this.family.find((user) => user.id === params.user_id);
+      let editingGift = this.findCustomGift(user);
+      axios
+        .patch(`/customgifts/${id}`, params)
+        .then((response) => {
+          let newGift = response.data;
+          console.log(1, newGift);
+          console.log(2, editingGift);
+          editingGift.id = newGift.id;
+          editingGift.note = newGift.note;
+          editingGift.user_id = newGift.user_id;
+          editingGift.customgift_purchaser = newGift.customgift_purchaser;
+          editingGift.customgift_purchaser_id = newGift.customgift_purchaser_id;
+        })
+        .catch((errors) => {
+          console.log("errors: ", errors);
+          console.log("errors: ", errors.response?.data?.errors);
         });
     },
   },
