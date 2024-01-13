7b05f7c95f6d60a921d9e7b7b1909f46f872780e
David Ryan <88855799+dave-ryan@users.noreply.github.com>
2024-01-13 16:06:20 -0600

    fix missing emit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -161,6 +161,7 @@
                     :deletingCustomGift="deletingCustomGift"
                     :associatedCustomGift="findCustomGift(secretSanta)"
                     @toggleCustomGiftCheckBox="toggleCustomGiftCheckBox"
+                    @editCustomGift="editCustomGift"
                   />
                 </div>
               </div>
