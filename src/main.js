239b991d64118d31fba5397100d0c0741a6ef17d
David Ryan <88855799+dave-ryan@users.noreply.github.com>
2024-01-13 00:35:15 -0600

    moving axios headers check to main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
+import VueCookies from "vue-cookies";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
+
+var jwt = VueCookies.get("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
