feea99cb364507b752d90ff7e4f72f3c0de106ff
David Ryan <88855799+dave-ryan@users.noreply.github.com>
2024-11-21 19:19:14 -0600

    content if current user is target user on share page

--- src/views/ShareList.vue
+++ src/views/ShareList.vue
@@ -19,8 +19,28 @@
         <div class="row">
           <div class="col"></div>
           <div class="col-lg-10">
-            <h2>{{ user.name }}'s List</h2>
-            <GiftList :user="user" />
+            <div v-if="currentUser?.id !== targetUser?.id">
+              <h2>{{ targetUser.name }}'s List</h2>
+              <GiftList :user="targetUser" />
+            </div>
+            <div v-if="currentUser?.id === targetUser?.id">
+              <h2>This is your share page</h2>
+              <div>
+                Copy this link to share with a friend, and they can see your
+                list, and if anyone purchased a gift
+              </div>
+              <transition name="fade">
+                <div
+                  v-if="copied"
+                  class="position-absolute start-50 translate-middle-x"
+                >
+                  Copied to your clipboard!
+                </div>
+              </transition>
+              <div class="mt-4">
+                <code class="fakelink" @click="copyLink()">{{ url }}</code>
+              </div>
+            </div>
           </div>
           <div class="col"></div>
         </div>
@@ -37,7 +57,18 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fakelink {
+  cursor: pointer;
+}
+.fade-leave-active {
+  transition: opacity 2s ease;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -51,47 +82,29 @@ export default {
   components: { Splash, Spinner, GiftList },
   data() {
     return {
-      user: null,
+      targetUser: null,
       pageLoaded: false,
       contentLoaded: false,
       splashSrc: christmasTree,
       unauthorized: false,
       currentUser: null,
+      url: "",
+      copied: false,
     };
   },
   created() {
-    const urlParams = new URLSearchParams(window.location.search);
-    let userId = urlParams.get("userId");
-    let params = {
-      share_code: urlParams.get("shareCode"),
-    };
-    axios
-      .get(`/users/${userId}`, { params })
-      .then((response) => {
-        this.user = response.data;
-        this.pageLoaded = true;
-        this.contentLoaded = true;
-      })
-      .catch((error) => {
-        if (error.response?.status === 401) {
-          this.unauthorized = true;
-          this.pageLoaded = true;
-          this.contentLoaded = true;
-        } else {
-          error.critical = true;
-          error.function = "shareMe";
-          this.$emit("onError", error);
-        }
-      });
+    this.url = window.location.href;
 
     axios
       .get("/current-user")
       .then((response) => {
         this.currentUser = response.data;
+        this.getUser();
       })
       .catch((error) => {
         if (error.response?.status === 401) {
           this.currentUser = null;
+          this.getUser();
         } else {
           error.critical = true;
           error.function = "getMe";
@@ -99,6 +112,39 @@ export default {
         }
       });
   },
-  methods: {},
+  methods: {
+    getUser() {
+      const urlParams = new URLSearchParams(window.location.search);
+      let userId = urlParams.get("userId");
+      let params = {
+        share_code: urlParams.get("shareCode"),
+      };
+      axios
+        .get(`/users/${userId}`, { params })
+        .then((response) => {
+          this.targetUser = response.data;
+          this.pageLoaded = true;
+          this.contentLoaded = true;
+        })
+        .catch((error) => {
+          if (error.response?.status === 401) {
+            this.unauthorized = true;
+            this.pageLoaded = true;
+            this.contentLoaded = true;
+          } else {
+            error.critical = true;
+            error.function = "shareMe";
+            this.$emit("onError", error);
+          }
+        });
+    },
+    copyLink() {
+      navigator.clipboard.writeText(this.url);
+      this.copied = true;
+      setTimeout(() => {
+        this.copied = false; //fade out
+      }, 200);
+    },
+  },
 };
 </script>
